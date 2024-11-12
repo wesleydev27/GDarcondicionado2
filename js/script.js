d80a802cb59378346344c8f0d6551be054e4283e
Wesley Vaz <wsbvaz@gmail.com>
2024-11-11 21:58:56 -0300

    gd versao nova

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Inicializar AOS (Animate On Scroll)
+AOS.init({
+    once: true,
+    offset: 50,
+    duration: 1000,
+});
+
+//CLICK MENU MOBILE
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+
+}
+
+
+//CARROSEL DE MARCAS
